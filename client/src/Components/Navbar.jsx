9d28a8eca2ea5cca19b4e13f547418a04757d936
yanakolajara <yanakolajara@pursuit.org>
2025-01-24 15:45:38 -0500

    Navbar rendering buttons with auth state

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import './Navbar.scss';
 
 export default function Navbar() {
+  const auth = false;
   return (
     <nav className='navbar'>
       <h1 className='navbar__header'>Statera</h1>
       <section className='navbar__routes'></section>
       <section className='navbar__auth'>
-        <button className='btn'>Log In</button>
-        <button className='btn'>Sign Up</button>
+        {auth ? (
+          <button className='btn'>Log Out</button>
+        ) : (
+          <>
+            <button className='btn'>Log In</button>
+            <button className='btn'>Sign Up</button>
+          </>
+        )}
       </section>
     </nav>
   );
