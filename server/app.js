c015d856fbf9639b6bf71e296f422f9da6d480b3
yanakolajara <yanakolajara@pursuit.org>
2025-01-29 21:31:15 -0500

    server fix

--- server/app.js
+++ server/app.js
@@ -5,8 +5,21 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(cors());
 app.use(express.json());
+
+const whitelist = ['http://localhost:3000', 'https://statera-app.netlify.app'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log('origin:', origin, 'not allowed');
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use('/auth', require('./controllers/authController'));
 app.use('/users', require('./controllers/usersController'));
