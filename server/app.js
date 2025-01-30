b7d2eac5f2dd63c5fd57cf57525d8f941e339eab
yanakolajara <yanakolajara@pursuit.org>
2025-01-29 22:50:17 -0500

    Update added new method to cors

--- server/app.js
+++ server/app.js
@@ -7,14 +7,32 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-// const whitelist = ['http://localhost:3000', 'https://statera-app.netlify.app'];
+// const whitelist = ['http://localhost:3000', 'https://statera-app.netlify.appify.app'];
+
 var corsOptions = {
   origin:
     process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000/'
-      : 'https://statera-app.netlify.app/',
+      ? 'http://localhost:3000'
+      : 'https://statera-app.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
+
+app.use((req, res, next) => {
+  const uri =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://statera-app.netlify.app';
+  res.header('Access-Control-Allow-Origin', uri); // Frontend URL
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(cors(corsOptions));
 
